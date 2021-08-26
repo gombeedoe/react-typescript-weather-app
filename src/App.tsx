3b8493e2db2a1904b7385f3d2406ddb3c88fb810
gombeedoe <gombeedoe@gmail.com>
2021-08-26 19:01:49 +0900

    初期実装

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,49 @@
+import React, { useState } from "react";
+import Title from './components/Title';
+import Form from './components/Form';
+import Results from './components/Results';
 import './App.css';
 
+type ResultsStateType = {
+  country: string;
+  cityName: string;
+  temperature: string;
+  conditionText: string;
+  icon: string;
+}
+
 function App() {
+
+  const [city, setCity] = useState<string>("");
+  const [results, setResults] = useState<ResultsStateType>({
+    country: "",
+    cityName: "",
+    temperature: "",
+    conditionText: "",
+    icon: ""
+  });
+  const getWeather = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    fetch(`https://api.weatherapi.com/v1/current.json?key=65ab532166534b8c8fe13652212508&q=${city}&aqi=no`)
+      .then(res => res.json())
+      .then(data => {
+        setResults({
+          country: data.location.country,
+          cityName: data.location.name,
+          temperature: data.current.temp_c,
+          conditionText: data.current.condition.text,
+          icon: data.current.condition.icon
+        })
+      });
+  };
+
   return (
-    <div className="App">
+    <div className="wrapper ">
+      <div className="container">
+        <Title />
+        <Form setCity={setCity} getWeather={getWeather} />
+        <Results results={results} />
+      </div>
     </div>
   );
 }
